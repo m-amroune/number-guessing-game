27c376d2e980c9c3511f052d9229a68732727eb8
Moustapha <amroune.moustapha@gmail.com>
2025-08-22 04:19:19 -0400

    feat: add prompt for guessing the secret number and read user input

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,23 @@ SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 # Prompt the user for a username
 echo "Enter your username:"
 read USERNAME
+
+# DB helper
+PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Check if user exists
+USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME';")
+
+if [[ -z $USER_DATA ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME');" > /dev/null
+else
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Guess prompt
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
+
